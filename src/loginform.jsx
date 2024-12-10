d329161f84e34591a6b1a85b86c7c4d96ad7fc04
Ben Thomas <Benjamingathomas@gmail.com>
2024-12-09 21:36:46 -0500

    added forms

---
+++ src/loginform.jsx
@@ -0,0 +1,20 @@
+const SignupLogin = () => {
+    <div className="flex justify-center items-center">
+        <div className="lg:w-[450px] bg-purple-500 py-2 px-10 rounded-bl-[40px]">
+            <h1 className="text-3xl text-red-500 text-center mb-8">Login</h1>
+            <div>
+                <div className="bg-blue-200 flex items-center gap-5" >
+                    <input type="text"></input>
+                </div>
+                <div className="bg-blue-200 flex items-center gap-5">
+                    <input type="email"></input>
+                </div>
+                <div className="bg-blue-200 flex items-center gap-5">
+                    <input type="Password"></input>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default SignupLogin
